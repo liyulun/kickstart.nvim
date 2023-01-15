dfdbf9c251eec8bd1ed0b315fd889631ce4b2a38
liyulun <12835041+liyulun@users.noreply.github.com>
2023-01-15 01:09:45 -0800

    Update config

--- init.lua
+++ init.lua
@@ -106,6 +106,13 @@ vim.o.incsearch = true
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- tab and indent 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+--vim.opt.smartindent = true
+
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
@@ -216,11 +223,11 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
+vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
+vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
@@ -437,6 +444,9 @@ vim.api.nvim_set_keymap('i', '<A-j>', '<ESC>:m .+1<CR>==gi', {})
 vim.api.nvim_set_keymap('i', '<A-k>', '<ESC>:m .-2<CR>==gi', {})
 vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", {})
 vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", {})
+
+-- key mapping to open file explore tree
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
